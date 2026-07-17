0e84cfcc0f7dfa1b00a50f2d82fe08f33d5c895c
mgowtham0704 <mgowtham0704@gmail.com>
2026-07-17 11:10:14 +0530

    deleted unwanted files

--- src/timeline.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-
-# Create 30 seconds of silence
-silence = AudioSegment.silent(duration=30000)
-
-# Save it
-silence.export("output/timeline.wav", format="wav")
-
-print("30-second silent audio created!")
